4288dda6ab78448287141a43ae6224f4ffec0033
Abner Marques de Oliveira <abner.oliveira-plu@parceiros-kroton.com.br>
2022-07-10 00:48:14 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -5,6 +6,7 @@ const bodyParser = require('body-parser');
 const controllers = require('./controller/controllers');
 const app = express();
 
+app.use(compression());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
