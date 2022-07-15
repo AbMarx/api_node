7096004b6019631a004f0a3d62b5245dc7aa5550
Abner Marques de Oliveira <abner.oliveira-plu@parceiros-kroton.com.br>
2022-07-14 23:10:05 -0300

    cacheamento da aplicação

---
+++ model/userModel.js
@@ -0,0 +1,9 @@
+class UserModel{
+    constructor(id,login,password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }   
+}
+
+module.exports =  UserModel
